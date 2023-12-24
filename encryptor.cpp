63b73501d8c0d0d69933e5bb42b4dc3eea8ed11d
ratoncaton <ratoncaton12@gmail.com>
2023-12-24 01:43:32 +0100

    file that will read and encrypt data from files, it haves terminal input test

---
+++ encryptor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/* Command line comprovation :)
+
+int main(int argc, char* argv[]){
+    if (argc >= 4){
+        std::cerr << "Input: " << argv[0];
+        return 1;
+    }
+
+    std::string arg1 = argv[1];
+    std::string arg2 = argv[2];
+
+    std::cout << arg1 << " " << arg2 << std::endl;
+    return 0;
+} */
+
+
