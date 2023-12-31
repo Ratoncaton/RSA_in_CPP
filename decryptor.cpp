77de439738d96ea8910a5fb50d3cf764d6eb3950
ratoncaton <ratoncaton12@gmail.com>
2023-12-31 01:19:08 +0100

    -fm and -f works

--- decryptor.cpp
+++ decryptor.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <cstring>
+#include <cstdlib>
 
 std::array<long long unsigned int, 2> specificOpenFile(std::string fileName){
     
@@ -17,24 +19,25 @@ std::array<long long unsigned int, 2> specificOpenFile(std::string fileName){
         return {1, 1};
     }
 
-    for(int i = 0; i <= 2;i++){
+    for(int i = 0; i < 2;i++){
         file >> key[i];
     }
     
     return key;
 }
 
-std::vector<int> openMessageFile(std::string fileName){
-    std::vector<int> message;
+std::vector<long long unsigned int> openMessageFile(std::string fileName){
+    std::vector<long long unsigned int> message;
     std::string path = "./" + fileName + ".msg";
     std::ifstream file(path);
 
     if(!file){
         return {1};
     }
-
-    for(int i = 0; i <= sizeof(file); i++){
-        file >> message[i];
+    
+    int num;
+    while (file >> num) {
+        message.push_back(num);
     }
 
     return message;
@@ -59,7 +62,7 @@ std::array<long long unsigned int, 2> openFile(){
             std::string line;
             std::array<long long unsigned int, 2> key;
 
-            for(int i = 0; i <= 2;i++){
+            for(int i = 0; i < 2;i++){
                file >> key[i];
                
             }
@@ -84,7 +87,7 @@ unsigned long long modPow(unsigned long long base, unsigned long long exp, unsig
     return result;
 }
 
-std::vector<char> Ascii_To_String(const std::vector<int>& message){
+std::vector<char> Ascii_To_String(const std::vector<long long unsigned int>& message){
     
     std::vector<char> ascii;
     
@@ -94,8 +97,8 @@ std::vector<char> Ascii_To_String(const std::vector<int>& message){
     return ascii;
 }
 
-std::vector<int> decryption(std::vector<int> cMessage, unsigned long long int n, unsigned long long int d){
-    std::vector<int> message;
+std::vector<long long unsigned int> decryption(std::vector<long long unsigned int> cMessage, unsigned long long int n, unsigned long long int d){
+    std::vector<long long unsigned int> message;
     for(int m : cMessage){
         message.push_back(modPow(m, d, n));
     }
@@ -103,9 +106,9 @@ std::vector<int> decryption(std::vector<int> cMessage, unsigned long long int n,
     return message;
 }
 
-std::vector<int> splitStringToInts(const std::string& str) {
+std::vector<long long unsigned int> splitStringToInts(const std::string& str) {
     std::istringstream iss(str);
-    std::vector<int> numbers;
+    std::vector<long long unsigned int> numbers;
     std::string word;
     while (iss >> word) {
         numbers.push_back(std::stoi(word));
@@ -117,66 +120,68 @@ int main(int argc, char* argv[]){
 
     std::array<long long unsigned int, 2> key;
     std::string cMessage = "";
-    std::vector<int> vCMessage;
-    std::vector<int> cMessageInts = {};
+    std::vector<long long unsigned int> vCMessage;
+    std::vector<long long unsigned int> cMessageInts;
 
     if(argc == 1){
         key = openFile();
     }
-    else if (argc > 2){
-        std::string option = argv[1];
-        if(option == "-f"){
-            key = specificOpenFile(argv[2]);
-        }
-        else if(option == "-m"){
-            vCMessage = openMessageFile(argv[2]);
-        }
-        else if(option == "-fm"){
-            key = specificOpenFile(argv[2]);
-            vCMessage = openMessageFile(argv[3]);
-        }
-        else if(option == "-h"){
-            std::cout << "option -h entered\n";
-            std::cout << "Usage: decryptor [OPTION] [FILE]\n";
-            std::cout << "Decrypts a message using the private key in the .priv file\n";
-            std::cout << "REMINDER:\n";
-            std::cout << "- The .priv file and .msg file has to be in the same directory as the program\n";
-            std::cout << "- It is not necessary to put the extension of the files\n";
+    else{
+        for (int i = 1; i < argc; ++i) {
+            if (std::strcmp(argv[i], "-f") == 0) {
+                key = specificOpenFile(argv[i + 1]);
+                break;
 
-            std::cout << "\n" << "Options:\n";
-            std::cout << "-f = specify a private key file name (.priv)\n";
-            std::cout << "If no option is given, the program will search for a .priv file in the current directory\n";
+            } else if (std::strcmp(argv[i], "-m") == 0) {
+                vCMessage = openMessageFile(argv[i + 1]);
+                key = openFile();
+                break;
 
-            std::cout << "\n" << "-m = specify a message file name (.msg)\n";
-            std::cout <<"If no option is given, the program will ask for a messsage, every message has to be separated with blank spaces\n";
+            } else if(std::strcmp(argv[i], "-fm") == 0){
+                key = specificOpenFile(argv[i + 1]);
+                vCMessage = openMessageFile(argv[i + 2]);
+                break;
 
-            std::cout << "\n" << "-fm = specify both a private key file name (.priv) and a message file name (.msg) in this order\n";
-            std::cout << "The order is: decryptor -fm [private key file name] [message file name]\n";
-            return 0;
-       }
-        else{
-            std::cout << "Invalid option, please use -h for help\n";
-            return 1;
+            } else if(std::strcmp(argv[i], "-h") == 0){
+                std::cout << "option -h entered\n";
+                std::cout << "Usage: decryptor [OPTION] [FILE]\n";
+                std::cout << "Decrypts a message using the private key in the .priv file\n";
+                std::cout << "REMINDER:\n";
+                std::cout << "- The .priv file and .msg file has to be in the same directory as the program\n";
+                std::cout << "- It is not necessary to put the extension of the files\n";
+
+                std::cout << "\n" << "Options:\n";
+                std::cout << "-f = specify a private key file name (.priv)\n";
+                std::cout << "If no option is given, the program will search for a .priv file in the current directory\n";
+
+                std::cout << "\n" << "-m = specify a message file name (.msg)\n";
+                std::cout <<"If no option is given, the program will ask for a messsage, every message has to be separated with blank spaces\n";
+
+                std::cout << "\n" << "-fm = specify both a private key file name (.priv) and a message file name (.msg) in this order\n";
+                std::cout << "The order is: decryptor -fm [private key file name] [message file name]\n";
+                return 0;           
+            }else{
+                std::cout << "Invalid option, please use -h for help\n";
+                return 1;
+            }
         }
-    
     }
 
     if (key[0] == 1 && key[1] == 1){
         std::cout << "Something has gone wrong, please check if the .priv file is in the folder";
         return 1;
     }
-
+    
     if(vCMessage.size() == 0){
     std::cout << "Enter the message you want to decrypt: ";
     std::getline(std::cin, cMessage);
 
     cMessageInts = splitStringToInts(cMessage);
-    }
-    else{
+    } else{
         cMessageInts = vCMessage;
     }
 
-    std::vector<int> aMessage = decryption(cMessageInts, key[0], key[1]);
+    std::vector<long long unsigned int> aMessage = decryption(cMessageInts, key[0], key[1]);
 
     std::vector<char> oMessage = Ascii_To_String(aMessage);
 
